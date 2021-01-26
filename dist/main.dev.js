f35b53ba313e487ebc0ac16b910f72bf3ca02e5e
Taizo Shimono <t_shimomo1407@yahoo.co.jp>
2021-01-26 23:18:40 +0900

    index.html create and base concept wright

--- dist/main.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-console.log(parseInt(2));
-console.log(parseInt(62));
-console.log(parseInt(0x2a));
-var num = 123;
-console.log(num.toString(2));
-console.log(num.toString(8));
-console.log(num.toString(16));
--- private_prop.js
+++
@@ -1,5 +0,0 @@
-import { MyClazz2 } from "./main.js";
-
-let c = new MyClazz2();
-console.log(c.getSecret());
-console.log(c[SEC_METHOD]());
